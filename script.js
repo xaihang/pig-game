50aaf9f5bf4a5d0dcf2cbc4a99707dab68972ecd
Xai Hang <xaihang12@gmail.com>
2023-01-13 16:01:02 -0600

    declared variable for dice elements and display

--- script.js
+++ script.js
@@ -5,14 +5,21 @@
 //__________________ global variables________________//
 //player 0 score: specific by id (#); El = element
 const score0El = document.getElementById('score--0'); 
+        //either one is OK// 
 // const score0 = document.querySelector('score--0');  
 
 //player 1 score: specific by id (#)  
 const score1El = document.getElementById('score--1'); 
 
-//  players score display at the very beginning: 
+// dice images 
+const diceEl = document.querySelector.apply('.dice'); 
+
+//  display settings at the very beginning or starting conditions: 
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden'); 
+
+
 
 
 //__________________ 9999999________________//
