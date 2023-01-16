d5cc0fadfc34aaee90c1e7bccf47029e740517de
Xai Hang <xaihang12@gmail.com>
2023-01-16 14:53:37 -0600

    added code for resetting the game

--- script.js
+++ script.js
@@ -135,4 +135,18 @@ btnHold.addEventListener('click', function() {
 });
 
 
+//resetting the game:
+btnNew.addEventListener('click', function() {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+})
+
+//set internal state to initial stage:
+
 
